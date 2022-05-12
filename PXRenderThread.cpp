0d8c16a8aecea93fe1335a942305daf6b7a31354
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-05-12 12:12:38 -0300

    Create a Thread class to process Pixmap

---
+++ PXRenderThread.cpp
@@ -0,0 +1,106 @@
+//
+// Created by Gustavo on 5/12/22.
+//
+
+#include "PXRenderThread.h"
+#include <QImage>
+#include <QPixmap>
+
+PXRenderThread::PXRenderThread(QObject *parent) : QThread(parent) { }
+
+PXRenderThread::~PXRenderThread() {
+    m_mutex.lock();
+    m_condition.wakeOne();
+    m_mutex.unlock();
+    wait();
+}
+
+void PXRenderThread::render(int brightness_factor, int contrast_factor, QSize size, QPixmap * orig_pixmap) {
+    QMutexLocker locker(&m_mutex);
+    
+    this->m_brightness_factor = brightness_factor;
+    this->m_contrast_factor = contrast_factor;
+    this->m_result_size = size;
+    this->m_original_pixmap = orig_pixmap;
+    
+    if (!isRunning()) {
+        start(LowPriority);
+    } else {
+        m_restart = true;
+        m_condition.wakeOne();
+    }
+}
+
+[[noreturn]] void PXRenderThread::run() {
+    forever {
+        m_mutex.lock();
+        const QSize result_size = this->m_result_size;
+        m_mutex.unlock();
+
+        QPixmap processed_pixmap =
+            m_original_pixmap->scaled(result_size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+        if (m_brightness_factor != 0) adjustBrightness(processed_pixmap);
+        if (m_contrast_factor != 0) adjustContrast(processed_pixmap);
+
+        if (!m_restart) emit renderedImage(processed_pixmap);
+
+        m_mutex.lock();
+        if (!m_restart) m_condition.wait(&m_mutex);
+        m_restart = false;
+        m_mutex.unlock();
+    }
+}
+
+void PXRenderThread::adjustBrightness(QPixmap& processed_pixmap) const {
+    // Convert the m_original_pixmap to QImage
+    QImage tmp = processed_pixmap.toImage();
+    
+    // Loop all the pixels
+    for (int y = 0; y < tmp.height(); y++) {
+        for (int x = 0; x < tmp.width(); x++) {
+            const auto old_color = tmp.pixelColor(x, y).convertTo(QColor::Hsl);
+            auto new_lightness = old_color.lightness() + m_brightness_factor;
+            if (new_lightness > 255) new_lightness = 255;
+            if (new_lightness < 0) new_lightness = 0;
+            const auto new_color = QColor::fromHsl(old_color.hue(), old_color.saturation(),
+                                                   new_lightness, old_color.alpha());
+            tmp.setPixelColor(x, y, new_color);
+        }
+    }
+    
+    processed_pixmap = QPixmap::fromImage(tmp);
+}
+
+void PXRenderThread::adjustContrast(QPixmap& processed_pixmap) const {
+    
+    // Convert the m_original_pixmap to QImage
+    QImage tmp = processed_pixmap.toImage();
+    int min_light = 10000;
+    int max_light = -10000;
+    
+    // Loop all the pixels and retrieve min/max lightness channel values
+    for (int y = 0; y < tmp.height(); y++) {
+        for (int x = 0; x < tmp.width(); x++) {
+            const int l_channel = tmp.pixelColor(x, y).convertTo(QColor::Hsl).lightness();
+            if (l_channel > max_light) max_light = l_channel;
+            if (l_channel < min_light) min_light = l_channel;
+        }
+    }
+    
+    // Loop all the pixels and update color
+    for (int y = 0; y < tmp.height(); y++) {
+        for (int x = 0; x < tmp.width(); x++) {
+            const auto old_color = tmp.pixelColor(x, y).convertTo(QColor::Hsl);
+            auto new_lightness = (255 + m_contrast_factor) *
+                                 (old_color.lightness() - min_light) / (max_light - min_light);
+            if (new_lightness > 255) new_lightness = 255;
+            if (new_lightness < 0) new_lightness = 0;
+            const auto new_color = QColor::fromHsl(old_color.hue(), old_color.saturation(),
+                                                   new_lightness, old_color.alpha());
+            tmp.setPixelColor(x, y, new_color);
+        }
+    }
+    
+    processed_pixmap = QPixmap::fromImage(tmp);
+}
---
+++ PXRenderThread.h
@@ -0,0 +1,42 @@
+//
+// Created by Gustavo on 5/12/22.
+//
+
+#ifndef PXRENDERTHREAD_H
+#define PXRENDERTHREAD_H
+
+#include <QMutex>
+#include <QSize>
+#include <QThread>
+#include <QWaitCondition>
+
+class PXRenderThread : public QThread {
+    Q_OBJECT
+
+public:
+    explicit PXRenderThread(QObject *parent = nullptr);
+    ~PXRenderThread() override;
+
+    void render(int brightnessFactor, int contrastFactor, QSize resultSize, QPixmap * orig_pixmap);
+
+signals:
+    void renderedImage(const QPixmap& result_pixmap);
+
+protected:
+    [[noreturn]] void run() override;
+
+private:
+    void adjustBrightness(QPixmap &processed_pixmap) const;
+    void adjustContrast(QPixmap &processed_pixmap) const;
+    
+    QPixmap *m_original_pixmap = nullptr;
+    QSize m_result_size;
+    int m_brightness_factor = 0.0;
+    int m_contrast_factor = 0.0;
+    
+    QMutex m_mutex;
+    QWaitCondition m_condition;
+    bool m_restart = false;
+};
+
+#endif // PXRENDERTHREAD_H
